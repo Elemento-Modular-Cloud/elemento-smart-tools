e6c5895284b8615d4337f0dd1ed989da2773cde8
Francesco Chpl <francesco@chpl.it>
2024-02-21 18:04:00 +0100

    feat: rdp service updated

--- rdp/rdp.js
+++ rdp/rdp.js
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2015 Sylvain Peyrefitte
+ *
+ * This file is part of mstsc.js.
+ *
+ * mstsc.js is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 const express = require('express')
 const http = require('http')
 const path = require('path')
@@ -5,12 +24,11 @@ const path = require('path')
 async function runRDPServer () {
   const app = express()
   app.use(express.static(path.join(__dirname, '/client')))
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/html/index.html'))
+  app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client', 'html', 'index.html'))
   })
-  console.log('Listening on port 9000')
-  const server = http.createServer(app).listen(9000)
-
+  const server = http.createServer(app).listen(process.env.PORT || 9000)
+  console.log('[RDP service] Listening on port 9000')
   require('./server/mstsc')(server)
 }
 

561a60c8d0325030205b0abe5b67b628409d4413
Francesco Chpl <francesco@chpl.it>
2024-02-22 09:36:38 +0100

    chore: clean up and build process

--- dist.sh
+++ dist.sh
@@ -1,3 +1,4 @@
 rm -rf ./out &&
 mkdir ./out &&
-npx pkg --compress GZip --output ./out/elemento-remote-tools-1.0.0 -t node18-linux,node18-macos,node18-win elemento-remote-tools.js
+# npx pkg --compress GZip --output ./out/elemento-remote-tools-1.0.0 -t node18-linux,node18-macos,node18-win elemento-remote-tools.js
+npx pkg .
